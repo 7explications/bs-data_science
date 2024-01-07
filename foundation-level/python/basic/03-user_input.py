226a82539ad8d2f14779357b6fe24df14822c891
7explications <jitendra.in@gmail.com>
2024-01-07 16:00:40 +0000

    user input

---
+++ foundation-level/python/basic/03-user_input.py
@@ -0,0 +1,11 @@
+import os #importing os library
+import re #importing  regular expressions.
+os.system('cls') # this line execte cls os command, anc will clear scree.
+
+#User input
+#The input() built-in function can be used to get data from the user. It also allows an optional string to make it an interactive process.
+name = input('what is your Name?:') #string input
+age = int(input('what is your Age?:')) #int input
+print("your name is:",name)
+print("and age is:",age)
+
