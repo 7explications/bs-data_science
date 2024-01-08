30745e4cbb89bcc9826fea994445ea1274688b70
7explications <jitendra.in@gmail.com>
2024-01-08 13:57:58 +0000

    user input type, numeric data type

--- foundation-level/python/basic/03-user_input.py
+++ foundation-level/python/basic/03-user_input.py
@@ -6,6 +6,12 @@ os.system('cls') # this line execte cls os command, anc will clear scree.
 #The input() built-in function can be used to get data from the user. It also allows an optional string to make it an interactive process.
 name = input('what is your Name?:') #string input
 age = int(input('what is your Age?:')) #int input
-print("your name is:",name)
-print("and age is:",age)
+print("your name is:", name)
+print("and age is:", age)
+
+#Type conversion
+#The type() built-in function can be used to know what data type you are dealing with. You can pass any expression as an argument
+print("tyoe of {} is {}".format(name, type(name)))
+print("tyoe of {} is {}".format(age, type(age)))
+print("tyoe of {}/3 is {}".format(age, type(age/3)))
 
---
+++ foundation-level/python/basic/04-numeric_data_types.py
@@ -0,0 +1,53 @@
+#Numeric data types
+#int : Integer numbers are made up of digits 0 to 9 and can be prefixed with unary operators like + or -. There is no restriction to the size of numbers that can be used, only limited by the memory available on your system
+a=40
+print(a)
+b=10_000_00
+print(b)
+print(a+10)
+print(b-29)
+
+# floating-point numbers
+f=3.14
+print(f)
+f=f-1
+print(f)
+
+#Arithmetic operators
+n1=11
+n2=2
+f1=23.5
+f2=3.3
+#All arithmetic operators you'd typically expect are available, If any operand is a floating-point number, result will be of float data type. Use + for addition, - for subtraction, * for multiplication and ** for exponentiation.
+# + for addition
+print("{} + {} = {}".format(n1,n2,(n1+n2)) )
+print("{} + {} = {}".format(f1,f2,(f1+f2)) )
+
+# - for subtraction
+print("{} - {} = {}".format(n1,n2,(n1-n2)) )
+print("{} - {} = {}".format(f1,f2,(f1-f2)) )
+
+# * for multiplication
+print("{} * {} = {}".format(n1,n2,(n1*n2)) )
+print("{} * {} = {}".format(f1,f2,(f1*f2)) )
+
+# ** for exponentiation
+print("{} ** {} = {}".format(n1,n2,(n1**n2)) )
+print("{} ** {} = {}".format(f1,f2,(f1**f2)) )
+
+
+#division
+#two operators for division. Use / if you want a floating-point result. Using // between two integers will give only the integer portion of the result (no rounding).
+# / if you want a floating-point result
+print("{} / {} = {}".format(n1,n2,(n1/n2)) )
+print("{} / {} = {}".format(f1,f2,(f1/f2)) )
+
+# // between two integers will give only the integer portion of the result (no rounding)
+print("{} // {} = {}".format(n1,n2,(n1//n2)) )
+print("{} // {} = {}".format(f1,f2,(f1//f2)) )
+
+#remainder
+#modulo operator % to get the remainder
+print("{} % {} = {}".format(n1,n2,(n1%n2)) )
+print("{} % {} = {}".format(f1,f2,(f1%f2)) )
+
